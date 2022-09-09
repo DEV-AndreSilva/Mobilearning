6b6fc7e0f6977d7013b699e0783053015ad7e078
Vinicius Assakawa <vinicius.assakawa@gmail.com>
2022-09-09 19:56:07 -0300

    Tela Professor

---
+++ mobilearning/lib/View/C_ProfessoresView.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CProfessores extends StatefulWidget {
+  const CProfessores({Key? key}) : super(key: key);
+
+  @override
+  State<CProfessores> createState() => _CProfessoresState();
+}
+
+class _CProfessoresState extends State<CProfessores> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+                child: Column(
+                children: <Widget>[
+                SizedBox(
+                  height: 500,
+                  width: 450,
+                  child: Image.asset('assets/images/IEI.png'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 4,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite seu nome";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.name,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: 'Nome Completo',
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite um E-mail";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: 'E-mail',
+                      prefixIcon: Icon(
+                        Icons.alternate_email,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite seu endereço: ";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.phone,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: 'Rua Machado de Assis 999',
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite seu telefone";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.phone,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: 'Telefone Ex: (17) 98888-8888',
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite sua senha";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: 'Senha',
+                      prefixIcon: Icon(
+                        Icons.vpn_key,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Digite seu CPF: ";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.phone,
+                    cursorColor: Colors.blue,
+                    decoration: const InputDecoration(
+                      hintText: '888.888.888-88',
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 25,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Confirmar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                ],
+            ),
+          ),
+    );
+}
+}
